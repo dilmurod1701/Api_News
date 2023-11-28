fcd79fb4c5df9ab09d682ae0baab996ec39d20fc
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-11-29 02:13:24 +0500

    Added: imported necessary packages in views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import login
+from rest_framework.generics import CreateAPIView
 
+from .serializers import LoginSerializer, SignupSerializer
 # Create your views here.
