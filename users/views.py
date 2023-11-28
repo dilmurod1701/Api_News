dcb30e9bf59485fb0ab45e5856087ca73f81667d
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-11-29 02:14:20 +0500

    Added: Login class to login

--- users/views.py
+++ users/views.py
@@ -6,3 +6,15 @@ from rest_framework.generics import CreateAPIView
 
 from .serializers import LoginSerializer, SignupSerializer
 # Create your views here.
+
+
+class LoginView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        serializer = LoginSerializer(data=self.request.data,
+                                     context={'request': self.request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response('Muvafaqiiyatli otdingiz')
