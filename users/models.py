f8460d47827b6aad204cb12116d8dad14724fcb6
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-11-29 02:01:47 +0500

    Added: Profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'profile'
+
+    def __str__(self):
+        return self.user.username
+    
