5550aa64ee4b7835b2b410c9dd026d2c39adc3d5
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-11-29 01:36:01 +0500

    Imported: necessary packages

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+import requests
+from bs4 import BeautifulSoup
 
+from .models import NewsData
+from .serializers import NewsSerializer
 # Create your views here.
