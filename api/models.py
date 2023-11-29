17f89fa08e624f1d0b34d9813088083ffab472ee
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-11-29 13:12:42 +0500

    Added: day field

--- api/models.py
+++ api/models.py
@@ -7,6 +7,7 @@ class NewsData(models.Model):
     title = models.CharField(max_length=2000)
     content = models.TextField(null=True)
     link = models.URLField(max_length=2000)
+    day = models.CharField(max_length=12)
 
     class Meta:
         db_table = 'news'
